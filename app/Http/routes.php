a41e6ca097cffc2e19862c46cbc29081e5d6a94c
te <longtranf@gmail.com>
2016-12-02 22:51:45 +0700

    Admin Interface

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -95,8 +95,5 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin']], function 
 
 // Categories
 Route::get('/test', function () {
-	Cart::add('192ao12', 'Product 1', 1, 9.99);
-	Cart::add('1239ad0', 'Product 2', 2, 5.95, ['size' => 'large']);
-
-	return view('test');
+	return view('admin.user_list');
 });
